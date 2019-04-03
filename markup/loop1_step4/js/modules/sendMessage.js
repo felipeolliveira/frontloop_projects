4b7db2312c5890c78d5bfda2a8044196e9070e3e
Felipe Oliveira <flpoliveira42@gmail.com>
2019-04-03 13:12:41 +0000

    Ajustes no Execício 4 - mudança dos balões de conversa para simular um chat padrão

--- markup/loop1_step4/js/modules/sendMessage.js
+++ markup/loop1_step4/js/modules/sendMessage.js
@@ -1,57 +1,63 @@
 export default function initSendMessage() {
+  const conversationArea_div = document.querySelector(".conversation-area");
+  const sendInput_input = document.querySelector(".send-input #input");
+  const buttonSendMessage = document.querySelector(".buttonsend");
+  const classMessage = "ballon-send";
 
-  const conversationArea_div = document.querySelector('.conversation-area');
-  const sendInput_input = document.querySelector('.send-input #input');
-  const buttonSendMessage = document.querySelector('.buttonsend');
-  const classMessage = 'ballon-receive';
-  
   const getTimeSendMessage = () => new Date();
-  const getTimeSendMessageHours = () => getTimeSendMessage().getHours().toString().padStart(2, '0');
-  const getTimeSendMessageMinutes = () => getTimeSendMessage().getMinutes().toString().padStart(2, '0');
-  
+  const getTimeSendMessageHours = () =>
+    getTimeSendMessage()
+      .getHours()
+      .toString()
+      .padStart(2, "0");
+  const getTimeSendMessageMinutes = () =>
+    getTimeSendMessage()
+      .getMinutes()
+      .toString()
+      .padStart(2, "0");
+
   const printMessage = () => {
     const textInputUser = sendInput_input.value;
-    const ballonSend = document.createElement('div');
-    const time = getTimeSendMessageHours() + ':' + getTimeSendMessageMinutes();
-    const span = document.createElement('span');
-    const textmessage = document.createElement('p');
-  
+    const ballonSend = document.createElement("div");
+    const time = getTimeSendMessageHours() + ":" + getTimeSendMessageMinutes();
+    const span = document.createElement("span");
+    const textmessage = document.createElement("p");
+
     ballonSend.classList.add(classMessage);
-    span.classList.add('time');
+    span.classList.add("time");
     span.innerText = time;
-    textmessage.classList.add('message');
+    textmessage.classList.add("message");
     textmessage.innerHTML = textInputUser;
-  
+
     ballonSend.appendChild(span);
     ballonSend.appendChild(textmessage);
-  
+
     conversationArea_div.appendChild(ballonSend);
-  }
-  
-  const autoScrollLastMessage = () =>{
+  };
+
+  const autoScrollLastMessage = () => {
     const lastMessage = conversationArea_div.lastChild;
     conversationArea_div.scrollTop = lastMessage.offsetTop;
-  }
-  
-  const cleanUserInput = () => sendInput_input.value = '';
-  
-  const handleKeyUp = (event) => {
-    if(sendInput_input.value && event.keyCode == 13) {
-      printMessage();
-      autoScrollLastMessage();
-      cleanUserInput();
-    }
-  }
-  
-  const handleClick = () => {
-    if(sendInput_input.value) {
-      printMessage();
-      autoScrollLastMessage();
-      cleanUserInput();
-    }
-  }
-  
-  buttonSendMessage.addEventListener('click', handleClick);
-  sendInput_input.addEventListener('keyup', handleKeyUp);
+  };
 
+  const cleanUserInput = () => (sendInput_input.value = "");
+
+  const handleKeyUp = event => {
+    if (sendInput_input.value && event.keyCode == 13) {
+      printMessage();
+      autoScrollLastMessage();
+      cleanUserInput();
+    }
+  };
+
+  const handleClick = () => {
+    if (sendInput_input.value) {
+      printMessage();
+      autoScrollLastMessage();
+      cleanUserInput();
+    }
+  };
+
+  buttonSendMessage.addEventListener("click", handleClick);
+  sendInput_input.addEventListener("keyup", handleKeyUp);
 }
